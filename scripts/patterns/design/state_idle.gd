5c50af2668efa5c2cde1698f23dde9f07244db83
pyroblast-enthusiast <pyroblast_enthusiast@protonmail.com>
2025-04-08 23:50:08 +0300

    update

--- scripts/patterns/design/state_idle.gd
+++ scripts/patterns/design/state_idle.gd
@@ -1,12 +1,16 @@
 class_name StateIdle
 extends State
 
+@export var walk_state: StateWalk
 
 func enter() -> void:
 	player.update_animation("idle")
 	
 	
 func process(_delta : float) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk_state
+	player.velocity = Vector2.ZERO
 	return null
 
 
---
+++ scripts/patterns/design/state_walk.gd
@@ -0,0 +1,37 @@
+class_name StateWalk
+extends State
+
+@export var idle_state: StateIdle
+@export var animation_name: String = ''
+@export var movement_speed: float = 100.0
+
+
+func enter() -> void:
+	player.update_animation(animation_name)
+	
+	
+func process(_delta : float) -> State:
+	if player.direction == Vector2.ZERO:
+		return idle_state
+	
+	player.velocity = player.direction * movement_speed
+	
+	if player.set_direction():
+		player.update_animation(animation_name)
+	return null
+
+
+func physics_process(_delta : float) -> State:
+	return null
+	
+
+func unhandled_input(event: InputEvent) -> State:
+	return null
+	
+
+#func next_transition() -> void:
+	#printerr("method not implemented")
+
+
+func exit() -> void:
+	printerr("method not implemented")
